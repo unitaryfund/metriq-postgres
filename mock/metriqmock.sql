fb8450472dbafa567fe03e2c6c1f63b1c42c60a8
Daniel Strano <stranoj@gmail.com>
2021-10-19 12:12:18 -0400

    DB mocking

---
+++ mock/metriqmock.sql
@@ -0,0 +1,12 @@
+-- Database: metriq
+
+-- DROP DATABASE metriq;
+
+CREATE DATABASE metriqmock
+    WITH 
+    OWNER = metriq
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
